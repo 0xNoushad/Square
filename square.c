fc08628b97457e5410013104947ce6221861a96d
Noushad Chaudhary <139552786+0xNoushad@users.noreply.github.com>
2023-09-25 19:48:12 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    float length, width, area;
+
+    
+    printf("area: ");
+    scanf("%f", &area);
+
+     
+
+   
+    area = area * area;
+
+   
+    printf("The area of the square is: %.2f\n", area);
+
+    return 0;
+}
